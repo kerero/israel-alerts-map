ef570cc36bd6b6c1951fcf1805505d94221abcf5
Ori K <orikerergil@gmail.com>
2021-05-18 08:24:25 +0000

    Fixed updateAlertsHistory.ts to handle no json data

--- src/data/updateAlertsHistory.ts
+++ src/data/updateAlertsHistory.ts
@@ -1,14 +1,19 @@
 // This is a server side script
 import axios from 'axios' // eslint-disable-line import/no-extraneous-dependencies
 import fs from 'fs'
-import alertHistory from './alertsHistory.json'
 
 const LANG = 'he'
 const url = `https://www.oref.org.il//Shared/Ajax/GetAlarmsHistory.aspx?lang=${LANG}&mode=3`
+const jsonPath = `${__dirname}/alertsHistory.json`
+
+if (!fs.existsSync(jsonPath)) {
+  fs.writeFileSync(jsonPath, '[{"datetime": "1970-01-01T00:00:00.000Z"}]')
+}
 
 axios.get(url).then((res) => {
+  const alertHistory = require(jsonPath) // eslint-disable-line
   const mostRecent = new Date(alertHistory[0].datetime)
   const newAlerts = (res.data as any[]).filter((v) => (new Date(v.datetime) > mostRecent))
   const mergedAlerts = newAlerts.concat(alertHistory)
-  fs.writeFileSync(`${__dirname}/alertsHistory.json`, JSON.stringify(mergedAlerts))
+  fs.writeFileSync(jsonPath, JSON.stringify(mergedAlerts))
 }).catch((e) => console.log(JSON.stringify(e))) // eslint-disable-line no-console
